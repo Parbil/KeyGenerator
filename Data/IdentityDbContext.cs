9567326b0db79d3e000e08911a48f1b36d293cba
Parbil <anwarp.net@gmail.com>
2019-01-14 11:45:06 +0600

    Missing Files

---
+++ Data/IdentityDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace BitproKeyGen.Data
+{
+    public class IdentityDbContext<T1, T2>
+    {
+    }
+}
---
+++ Models/ApplicationRole.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BitproKeyGen.Models
+{
+    public class ApplicationRole :IdentityRole
+    {
+        public ApplicationRole() : base() { }
+        public ApplicationRole(String roleName) : base(roleName)
+        {
+
+        }
+    }
+}
